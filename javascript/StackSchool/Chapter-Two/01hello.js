be5c3b1ef4830eefe1dd2ef0d1dbb8ae35fde289
Saiful Islam Shanto <52715000+Helloshanto@users.noreply.github.com>
2020-05-03 14:50:49 +0600

    Add files via upload

---
+++ javascript/StackSchool/Chapter-Two/01hello.js
@@ -0,0 +1,6 @@
+console.log("Hello world")
+console.log("My Name Is " + "Saiful islam Shanto")
+
+// string + number => string
+console.log(typeof('My Fav NO: ' + 9))
+console.log('9' + 9)
---
+++ javascript/StackSchool/Chapter-Two/02Variable.js
@@ -0,0 +1,39 @@
+// Varibale
+
+// var name = "Saiful Islam Shanto"
+// console.log(name + " Learning Javascript 2/5/2020 ")
+
+// var age = 25
+// console.log("My Age is : " + age)
+
+/* 
+
+// variable in depth
+
+    // var , let , conts
+
+    // var => Function Scoped
+    var name = "Shanto"
+    console.log(name)
+
+    // let => es6 Global Scoped
+    let age = 10
+    console.log(age)
+
+    //initicalization variable
+    let address;
+    address = "Dhaka,Bangladesh"
+    console.log(address)
+
+*/
+
+//  Variable Naming Conversation;
+/* 
+
+*/
+let firstName; // Camel Case --> Recomanded
+let firstName99;
+let first_name;
+let FirstName; //pascal case => object , Constructor function , ES6 class
+
+
