77807daf396bbfceaaa4bcdc80e0b2d795b3a496
Saiful Islam Shanto <52715000+Helloshanto@users.noreply.github.com>
2020-05-05 11:38:12 +0600

    Add files via upload

---
+++ javascript/StackSchool/Chapter-Two/03datatype.js
@@ -0,0 +1,66 @@
+// 009. What is Data Types in Javascript
+
+//  DataType
+    /*
+        * ##### Primitive
+            * Number => 10 1.2 4.1416
+            * String => "" + ''
+            * Boolean => true / false
+            * Null => value dawa hoynai.
+            * undefind => difind kora hoynai
+            * Nan
+            * infinity
+        * ##### Object
+            * Array
+            * Function 
+            * object
+     */
+
+
+// Primitive Datatype => in depth
+
+// ****** Number *******
+/*
+    var age = 21 //intager
+    var pi = 3.1416 //float
+    var nn = Number(45)
+    var nnn = Number('45')
+    console.log(nn)
+    console.log(Number.parseInt(pi))
+    console.log(Number.parseFloat(nn))
+    console.log(Number.MAX_VALUE)
+    console.log(Number.MIN_VALUE)
+
+    console.log(1/0) // infinity datatype
+    console.log('abc' * 10) // NaN datatype
+*/
+
+
+// 011. Basic Strings in Javascript
+// ****** String *******
+
+// javascript string literal var str - str3 
+    /*  
+        var str = 'Shanto'
+        var str2 = "shanto"
+        var str3 = `String`
+
+
+        var str4 = String('Shanto')
+        var str5 = String(41)
+        var str6 = String(3.1416)
+
+        console.log(str4,str5,str6)
+    */
+
+
+// 012. Basic Booleans in Javascript
+
+// ****** Bollean *******
+
+var bol1 = true
+var bol2 = false
+var bol3= Boolean(true)
+var bol4= Boolean(false)
+
+console.log(bol1,bol2 , bol3,bol4)
