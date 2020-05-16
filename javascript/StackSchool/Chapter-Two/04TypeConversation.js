f698db0a0841fe04b7b1166d44db81085dc03961
Saiful Islam Shanto <52715000+Helloshanto@users.noreply.github.com>
2020-05-16 12:18:55 +0600

    Add files via upload

---
+++ javascript/StackSchool/Chapter-Two/04TypeConversation.js
@@ -0,0 +1,31 @@
+// 04TypeConversation.js
+//Conver datatype
+
+
+// var str = '100'
+// var n = 10
+
+// console.log(typeof(str))
+// console.log(typeof(n))
+
+
+// console.log(Number(str))
+// console.log(n.toString())
+
+// console.log(typeof(str))
+// console.log(typeof(n))
+
+
+// // var str = '1000'
+// // var n = 10
+
+// // console.log(str*n)
+
+
+// console.log(Boolean(''))
+// console.log(Boolean(""))
+
+
+
+// 015. Octal and Hexadecimal Number in Javascript
+
