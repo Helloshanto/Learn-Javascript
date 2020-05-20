65045f3a08cb960cd2f788983f5a840fa466dc3d
Helloshanto <shantoprogrammer@gmail.com>
2020-05-20 12:49:24 +0600

    operator

--- javascript/StackSchool/Chapter-Two/05Operators.js
+++ javascript/StackSchool/Chapter-Two/05Operators.js
@@ -8,16 +8,80 @@
  * %
  * ++
  * --
- */ 
+ */
+/**
+var a = 10
+var b = 2
 
- var a = 10
- var b = 2
-
- console.log(' + ' + ( a+b))
- console.log(' - ' + ( a-b))
- console.log(' * ' + ( a*b))
- console.log(' / ' + ( a/b))
- console.log(' % ' + ( a%b))
+    console.log(' + ' + ( a+b))
+    console.log(' - ' + ( a-b))
+    console.log(' * ' + ( a*b))
+    console.log(' / ' + ( a/b))
+    console.log(' % ' + ( a%b))
+ */
 
 
+/*
+//incemental
+//++ pre incremental - post incremental++
 
+//++ pre incremental
+console.log(b);
+console.log(++b);
+
+//post incremental
+console.log(a++);
+console.log(a);
+
+*/
+
+
+/*****************************/
+
+// JavaScript Assignment Operators
+/**
+ * =   => a=b 
+ * +=  => 
+ * -=
+ * *=
+ * /=
+ * %=
+ * **=
+*/
+
+var a = 20;
+var b = 10;
+console.log(a + b)
+
+// +=
+a += b; //=> a=a+b
+console.log(a)
+
+//-=
+a -= b;;
+console.log(a);
+
+//-=
+a *= b;;
+console.log(a);
+
+// /=
+a /= b;
+console.log(a);
+
+
+// %=
+a %= b;
+console.log(a);
+
+
+// **=
+
+var x = 2;
+var y = 5;
+
+x **= y;
+console.log(x);
+
+
+// 
