d801558a6ff61bc082e53c7245725f57effe25b7
Helloshanto <shantoprogrammer@gmail.com>
2020-05-20 13:19:59 +0600

    operator

--- javascript/StackSchool/Chapter-Two/05Operators.js
+++ javascript/StackSchool/Chapter-Two/05Operators.js
@@ -40,8 +40,8 @@ console.log(a);
 
 // JavaScript Assignment Operators
 /**
- * =   => a=b 
- * +=  => 
+ * =   => a=b
+ * +=  =>
  * -=
  * *=
  * /=
@@ -49,6 +49,7 @@ console.log(a);
  * **=
 */
 
+/**
 var a = 20;
 var b = 10;
 console.log(a + b)
@@ -82,6 +83,38 @@ var y = 5;
 
 x **= y;
 console.log(x);
+ */
 
 
-// 
+
+// JavaScript Comparison Operators
+/**
+ * ==  
+ * ===
+ * !=
+ * !==
+ * >
+ * <
+ * >=
+ * <=
+ * ?
+ * NOTE : its return boolen value (true/ false)
+*/
+
+var a = 10;
+var b = 20;
+console.log(a == b); //false => == equal
+console.log(a != b); //true => != not equal
+
+console.log(a < b); //true => < less Then => a=10 < b=20 -> true
+console.log(a > b); //False => > Gratter Then =>  a=10 > b=20 => false
+
+a = 20; //change var
+console.log(a <= b); //true => < less Then or equal
+console.log(a >= b); //true => > Gratter Then or equal
+
+var x = '50'
+var y = 50
+
+console.log(x === y); // false => check equal and datatype
+console.log(x !== y); // True => check not equal and not same datatype
