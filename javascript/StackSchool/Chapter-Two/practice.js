dd4da97b1e94934d44e2a2684e4a0668df98be3c
Helloshanto <shantoprogrammer>
2020-05-22 18:23:10 +0600

    practice

--- javascript/StackSchool/Chapter-Two/practice.js
+++ javascript/StackSchool/Chapter-Two/practice.js
@@ -1,1 +1,20 @@
 // opretatos
+
+
+var a = 10
+var b = 20
+console.log(a == b);
+console.log(a != b);
+
+console.log(a > b);
+console.log(a > b);
+
+var j = 20;
+var s = 20;
+console.log(j >= s) // false
+console.log(j <= s) // true
+
+var c = '50'
+var d = 50
+console.log(c === d); //false
+console.log(c !== d); //false
