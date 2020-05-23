668347b82e107492ee8058921d8f5d5ee439e694
Helloshanto <shantoprogrammer>
2020-05-24 00:04:27 +0600

    math function

---
+++ javascript/StackSchool/Chapter-Two/06math.js
@@ -0,0 +1,20 @@
+// 018. Math Functions Provided by Javascript
+
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 4.589
+console.log(Math.abs(n)) //for exaxt number => 4
+console.log(Math.floor(n)) // show intager , dosomik er por skip korbe => 4
+console.log(Math.ceil(n)) //show itager number but kacha kachi value //Output => 5
+console.log(Math.round(n)) // => ata avarge number dey 1.5 hole 2 dey ar 1.3 hole 1 dey
+
+console.log(Math.max(400, 500)) // check maximum number 500
+console.log(Math.min(400, 500)) // check minimum number 400
+
+console.log(Math.pow(2, 3)) //power => 8
+
+console.log(Math.sqrt(9)) //squar root => 3
+
+console.log(Math.random()) //its show random number
+console.log(Math.round(Math.random() * 100 + 99)) //its show random number with in 100
--- javascript/StackSchool/Chapter-Two/practice.js
+++ javascript/StackSchool/Chapter-Two/practice.js
@@ -1,20 +1,26 @@
 // opretatos
 
+/*
+    var a = 10
+    var b = 20
+    console.log(a == b);
+    console.log(a != b);
 
-var a = 10
-var b = 20
-console.log(a == b);
-console.log(a != b);
+    console.log(a > b);
+    console.log(a > b);
 
-console.log(a > b);
-console.log(a > b);
+    var j = 20;
+    var s = 20;
+    console.log(j >= s) // false
+    console.log(j <= s) // true
 
-var j = 20;
-var s = 20;
-console.log(j >= s) // false
-console.log(j <= s) // true
+    var c = '50'
+    var d = 50
+    console.log(c === d); //false
+    console.log(c !== d); //false
+*/
 
-var c = '50'
-var d = 50
-console.log(c === d); //false
-console.log(c !== d); //false
+// math Function Practice
+
+console.log(Math.round(Math.PI))
+
